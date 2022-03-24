08124c91d15ec79e6de0880703b3b972f613d6ba
Chimoha31 <mokochii1108@gmail.com>
2022-03-24 11:40:57 -0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from "./Home";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div className="App">
+      <Link to="/">Home</Link>
+      <Link to="/page1">Page1</Link>
+      <Link to="/page2">Page2</Link>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/page1" element={<Page1 />}></Route>
+        <Route path="/page2" element={<Page2 />}></Route>
+      </Routes>
+      </div>
+    </Router>
+ 
   );
 }
 
